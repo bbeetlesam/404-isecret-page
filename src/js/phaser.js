aacbef790308ae4e28dd4a90924264caabee84d8
bbeetlesam <samsismines@gmail.com>
2025-03-21 14:21:41 +0700

    refactor: rename from main.js to phaser.js

---
+++ src/js/phaser.js
@@ -0,0 +1,32 @@
+import Phaser from "phaser";
+import { MainScene } from "./scene.js";
+
+// const isLandscape = window.innerWidth > window.innerHeight;
+
+export const config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: false,
+        antialias: true,
+    },
+    scale: {
+        parent: "gameId",
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1440,
+        height: 1000,
+        // min: {
+        //     width: 320,
+        //     height: 400
+        // },
+        // max: {
+        //     width: 1600,
+        //     height: 1600
+        // },
+        zoom: 1.5
+    },
+    scene: [MainScene],
+    backgroundColor: "#00377a",
+};
+
+export const game = new Phaser.Game(config);
