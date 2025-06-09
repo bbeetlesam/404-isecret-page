42a28e5adfb28b1373453139a7458557de192e5a
bbeetlesam <samsismines@gmail.com>
2025-06-09 11:29:05 +0700

    feat: add physics and demos making it look like the actual game's design

--- src/js/scene.js
+++ src/js/scene.js
@@ -1,52 +1,56 @@
-// Description: Main Scene
+// Main Scene
+import { GameState } from './gameState.js';
+
 export class MainScene extends Phaser.Scene {
     constructor()
     {
         super("MainScene");
     }
     
+    getSceneSize() {
+        return {
+            width: this.scale.width,
+            height: this.scale.height
+        };
+    }
+    
     // Load assets
     preload()
     {
-        // this.load.image("sky", "https://labs.phaser.io/assets/skies/space3.png");
-        this.load.image("sky", "/img/superunknown.jpeg");
+        this.load.image("ground", "/img/superunknown.jpeg");
         this.load.svg({key: "tes", url: "./img/icon.svg", svgConfig: {width: 800, height: 800}});
     }
     
     // Create game objects
     create()
     {
-        const img = this.add.image(this.scale.width/2, this.scale.height/2, 'sky');
-        img.setScale(1.0);
+        const sceneSize = this.getSceneSize();
+        let width = sceneSize.width;
+        let height = sceneSize.height;
         
-        const text = this.add.text(this.scale.width/2, this.scale.height/2, 'Shangri-La', {
-            fontSize: '80px',
-            color: '#cecece',
+        const text = this.add.text(10, -5, GameState.score.toString(), {
+            fontSize: '85px',
+            color: '#ffffff',
             fontFamily: 'Clear Sans',
-            resolution: 2,
-            padding: { x: 1, y: 1}
-        });
-        text.setOrigin(0.5);
-        
-        this.input.keyboard.on('keydown-SPACE', () => {
-            text.setStyle({fontSize: '100px', color: '#00ff00'});
-            // document.getElementById('p').innerHTML = "Paragraph changed.";
+            resolution: 0,
         });
         
-        this.input.keyboard.on('keyup-SPACE', () => {
-            text.setStyle({fontSize: '80px', color: '#fcba03'});
-            // document.getElementById('p').innerHTML = "halo ges";
-        });
-
-        this.input.on('pointerdown', (pointer) => {
-            text.setStyle({fontSize: '125px', color: '#00ff00'});
-            console.log('Clicked/Touched at:', pointer.x, pointer.y);
-        });
-
-        this.input.on('pointerup', (pointer) => {
-            text.setStyle({fontSize: '100px', color: '#fcba03'});
-            console.log('Clicked/Touched at:', pointer.x, pointer.y);
-        });
+        this.grounds = this.physics.add.staticGroup();
+        
+        for (let i = 0; i < width/60; i++) {
+            for (let j = 0; j < height/60 - 13; j++) {
+                const ground = this.grounds.create(i * 60, 60 * (13 + j), 'ground');
+                ground.setDisplaySize(60, 60);
+                ground.setOrigin(0, 0);
+                ground.refreshBody();
+            }
+        }
+        
+        this.player = this.add.circle(100, 100, 35, 0xffffff);
+        this.physics.add.existing(this.player);
+        this.player.body.setBounce(0.4);
+        
+        this.physics.add.collider(this.player, this.grounds);
     }
 
     // Update game state per frame
