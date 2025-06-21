1ecccb4bbbb94ff8f7068528b1e5d5dc85890f97
bbeetlesam <samsismines@gmail.com>
2025-06-21 23:00:47 +0700

    add: click outside canvas to hide game refactor: in main.js, combine codes that share same js event, remove hide button

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,43 @@
 // JawaScript main entry
 
-import { config, game } from './phaser.js';
+import { game } from './phaser.js';
 import GameState from './gameState.js';
 
 window.addEventListener('resize', () => {
     game.scale.refresh();
 });
 
-document.getElementById('showGameButton').addEventListener('click', () => {
-    GameState.isShown = true;
-});
-
+// Track keydown events
 document.addEventListener('keydown', (e) => {
+    // If the ESCAPE key is pressed (while the game is shown)
     if (e.key === 'Escape' && GameState.isShown) {
-        GameState.isShown = false;
         const mainScene = game.scene.getScene('MainScene');
+        
+        GameState.isShown = false;
         if (mainScene && typeof mainScene.quitGame === 'function') {
             mainScene.quitGame();
+        }
+    }
+});
+
+// Track click events
+document.addEventListener('click', (e) => {
+    // Show the canvas if clicked on the button
+    if (e.target.id === 'showGameButton') {
+        GameState.isShown = true;
+    }
+    
+    // Close game if click outside canvas while the canvas is shown
+    if (e.target.id === 'game-id') {
+        const canvas = document.querySelector('#game-id canvas');
+        
+        if (!(canvas && canvas.contains(e.target))) {
+            const mainScene = game.scene.getScene('MainScene');
+            
+            GameState.isShown = false;
+            if (mainScene && typeof mainScene.quitGame === 'function') {
+                mainScene.quitGame();
+            }
         }
     }
 });
